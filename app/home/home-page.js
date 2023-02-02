345f99588c2c4e98fc1d5dabe50227b2e7cdae61
LeannaGutierrez <122936764+LeannaGutierrez@users.noreply.github.com>
2023-02-02 15:42:51 -0600

    3 files modified

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -1,12 +1,15 @@
-import { Frame } from '@nativescript/core';
+import { Dialogs, getViewById } from '@nativescript/core';
 
-export function Click() {
-  var navigationEntry = {
-    moduleName: './info/info-page',
-    transition: {
-      name: 'slideUp',
-      duration: 1000,
-    },
-  };
-  Frame.topmost().navigate(navigationEntry);
+export function onSubmit(args) {
+  var button = args.object;
+  var parent = button.parent;
+
+  if(parent)
+  {
+    var TxtView = getViewById(parent, "msg"); 
+    if(TxtView) 
+    {
+      Dialogs.alert("Your message" + TxtView.text + "has been sent"); 
+    }
+  }
 }
